81a44626ad9027ca5bc6d999b87eafd4d39dae2c
franborn <37807985+franborn@users.noreply.github.com>
2025-08-20 19:24:08 +0200

    Update settings.py

--- bioweb/settings.py
+++ bioweb/settings.py
@@ -11,44 +11,23 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os, sys
-
-if LOG_TO_STDOUT:
-    LOGGING = {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "handlers": {
-            "console": {"class": "logging.StreamHandler", "stream": sys.stdout},
-        },
-        "root": {"handlers": ["console"], "level": os.getenv("DJANGO_LOG_LEVEL", "INFO")},
-        "loggers": {
-            "django": {"handlers": ["console"], "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"), "propagate": False},
-        },
-    }
-else:
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    os.makedirs(os.path.join(os.path.dirname(BASE_DIR), "logs"), exist_ok=True)
-    LOGGING = {
-        "version": 1,
-        "disable_existing_loggers": False,
-        "handlers": {
-            "file": {
-                "class": "logging.FileHandler",
-                "filename": os.path.join(os.path.dirname(BASE_DIR), "logs", "django.log"),
-            },
-        },
-        "root": {"handlers": ["file"], "level": "INFO"},
-        "loggers": {"django": {"handlers": ["file"], "level": "INFO", "propagate": False}},
-    }
+from pathlib import Path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+
+
 
 LOG_TO_STDOUT = os.environ.get("LOG_TO_STDOUT") == "1"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '))om(68r(=jl)lzj#70zkhv*h8x25h9ga$w*i($&r6fsup#tm%'
+#SECRET_KEY = '))om(68r(=jl)lzj#70zkhv*h8x25h9ga$w*i($&r6fsup#tm%'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "insecure-dev-key")
+DEBUG = os.environ.get("DEBUG", "0") == "1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -56,7 +35,8 @@ DEBUG = False
 ALLOWED_HOSTS = ['localhost',
                  '127.0.0.1',
                  '.coursera-apps.org',
-                 '.coursera.org', ]
+                 '.coursera.org',
+                '']+ os.environ.get("ALLOWED_HOSTS", "").split()
 
 # Application definition
 
@@ -155,7 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = BASE_DIR / "staticfiles"
+#STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGGING = {
     'version': 1,
@@ -187,3 +168,44 @@ LOGGING = {
         },
     },
 }
+LOG_TO_STDOUT = os.environ.get("LOG_TO_STDOUT", "1") == "1"  # default to stdout on Heroku
+
+if LOG_TO_STDOUT:
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "handlers": {
+            "console": {"class": "logging.StreamHandler", "stream": sys.stdout},
+        },
+        "root": {"handlers": ["console"], "level": os.getenv("DJANGO_LOG_LEVEL", "INFO")},
+        "loggers": {
+            "django": {
+                "handlers": ["console"],
+                "level": os.getenv("DJANGO_LOG_LEVEL", "INFO"),
+                "propagate": False,
+            },
+        },
+    }
+else:
+    # Local/dev file logging (project folder, not /var/log)
+    log_dir = BASE_DIR / "logs"
+    os.makedirs(log_dir, exist_ok=True)
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "handlers": {
+            "file": {
+                "class": "logging.FileHandler",
+                "level": "DEBUG",
+                "filename": str(log_dir / "django.log"),
+            },
+        },
+        "root": {"handlers": ["file"], "level": "INFO"},
+        "loggers": {
+            "django": {
+                "handlers": ["file"],
+                "level": "DEBUG",
+                "propagate": False,
+            },
+        },
+    }
